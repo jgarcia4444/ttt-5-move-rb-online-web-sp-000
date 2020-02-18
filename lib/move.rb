7a759a764a0b54e425a701c7e05a0ed38d861d58
Jake Garcia <bballwiz4@gmail.com>
2020-02-18 16:01:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,3 +11,8 @@ def input_to_index(move_input)
   index = move_input.to_i - 1
   return index
 end
+
+def move(board, player_move, player_char="X")
+  board[player_move] = player_char
+  return board
+end
